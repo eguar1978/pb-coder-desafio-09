caf76db9d8545c0ba19a22464e77e2392d34038a
Andres <maileguar@gmail.com>
2021-06-08 23:00:45 -0300

    Entrega de apuro

--- api/productos.js
+++ api/productos.js
@@ -1,28 +1,66 @@
 class Productos {
+  constructor() {
+    this.productos = []
+    this.currentId = 0
+  }
 
-    constructor() {
-        this.productos = []
-    }
+  obtener(id) {
+    const producto = this.productos.find(prod => prod.id === id)
+    return producto
+  }
 
-    obtener(id) {
-        const producto = this.productos.find(prod => prod.id === id)
-        return producto
-    }
+  guardar(producto) {
+    // aumenta el currentId en cada carga de productos
+    this.currentId += 1
+    producto.id = this.currentId
+    this.productos.push(producto)
+    return producto
+  }
 
-    guardar(producto) {
+  listar() {
+    return this.productos
+  }
 
-        producto.id = this.productos.length + 1
-        this.productos.push(producto)
+  actualizar(id, data) {
+    let productoActualizado = {}
+    let existe = false
 
+    this.productos = this.productos.map(prod => {
+      if (prod.id === id) {
+        existe = true
+        console.log(data)
+        prod = { ...prod, ...data }
+        productoActualizado = prod
+        return productoActualizado
+      } else {
+        return prod
+      }
+    })
 
-        return producto
-    } 
+    if (!existe) throw new Error('No se encontro un producto con ese id')
 
-    listar() {
-        return this.productos
-    }
+    return productoActualizado
+  }
 
+  borrar(id) {
+    let existe = false
+
+    let productoEliminado = {}
+
+    this.productos = this.productos.filter(prod => {
+      if (prod.id !== id) {
+        return prod
+      } else {
+        existe = true
+        productoEliminado = prod
+      }
+    })
+
+    if (!existe) throw new Error('No se encontro un producto con ese id')
+
+    return productoEliminado
+  }
 }
 
 // exporto una instancia de la clase
-module.exports = new Productos();
+module.exports = new Productos()
